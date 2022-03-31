46163953d0169cb0ae57d1edee19afaea64649de
Royalboe <auomidejohnson@gmail.com>
2022-03-31 09:35:52 +0100

    Made changes and implemented data binding

--- app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListFragment.kt
+++ app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.royalboe.bakingtutorial.R
 import com.royalboe.bakingtutorial.databinding.FragmentFirstBinding
@@ -20,12 +21,13 @@ class FirstFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val viewModel: RecipeListViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater)
         return binding.root
 
     }
