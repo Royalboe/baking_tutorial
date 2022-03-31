46e9f5410ccaad4e37a250734d3cb5995e988b23
Royalboe <auomidejohnson@gmail.com>
2022-03-31 09:06:25 +0100

    made changes

--- app/src/main/java/com/royalboe/bakingtutorial/FirstFragment.kt
+++ app/src/main/java/com/royalboe/bakingtutorial/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.royalboe.bakingtutorial
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.royalboe.bakingtutorial.databinding.FragmentFirstBinding
 
@@ -22,7 +22,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -35,7 +35,9 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
