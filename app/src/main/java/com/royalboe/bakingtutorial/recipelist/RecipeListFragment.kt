67eaf8dd22c2da460a909e57dbb4e1447616ab2b
Royalboe <auomidejohnson@gmail.com>
2022-04-02 19:15:32 +0100

    Created a function to initialize the player

--- app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListFragment.kt
+++ app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListFragment.kt
@@ -1,5 +1,6 @@
 package com.royalboe.bakingtutorial.recipelist
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.Util
 import com.royalboe.bakingtutorial.R
 import com.royalboe.bakingtutorial.databinding.FragmentFirstBinding
 
@@ -16,6 +20,9 @@ import com.royalboe.bakingtutorial.databinding.FragmentFirstBinding
 class RecipeListFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private var player: ExoPlayer? = null
+    private val contexts = requireContext()
+    private val uri: Uri = Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -44,6 +51,49 @@ class RecipeListFragment : Fragment() {
 
     }
 
+    fun initializePlayer(){
+        player = ExoPlayer.Builder(contexts)
+            .build()
+            .also {
+                binding.videoPlayer.player = it
+                val mediaItem = MediaItem.fromUri(uri)
+                it.setMediaItem(mediaItem)
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) initializePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT < 24 || player == null) initializePlayer()
+    }
+
+    private fun hideSystemUi() {
+        binding.videoPlayer.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) releasePlayer()
+    }
+
+    private fun releasePlayer() {
+        TODO("Not yet implemented")
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
