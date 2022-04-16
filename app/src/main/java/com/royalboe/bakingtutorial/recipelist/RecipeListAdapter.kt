f2eb81624bb95eba3076b4d72cf0fe206bd67a0b
Royalboe <auomidejohnson@gmail.com>
2022-04-16 14:16:41 +0100

    Added recyclerview

--- app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListAdapter.kt
+++ app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListAdapter.kt
@@ -1,4 +1,24 @@
 package com.royalboe.bakingtutorial.recipelist
 
-class RecipeListAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecipeListAdapter: RecyclerView.Adapter<RecipeListAdapter.RecipeListViewHolder>() {
+
+    class RecipeListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecipeListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
