71c5605d519ac3d2935ead88a30352e1df6b8117
Royalboe <auomidejohnson@gmail.com>
2022-04-16 14:46:40 +0100

    WIP Adapter

--- app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListAdapter.kt
+++ app/src/main/java/com/royalboe/bakingtutorial/recipelist/RecipeListAdapter.kt
@@ -1,20 +1,26 @@
 package com.royalboe.bakingtutorial.recipelist
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.royalboe.bakingtutorial.databinding.RecipeListViewBinding
+import com.royalboe.bakingtutorial.network.Recipe
 
 class RecipeListAdapter(private val viewModel: RecipeListViewModel): RecyclerView.Adapter<RecipeListAdapter.RecipeListViewHolder>() {
 
-    class RecipeListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class RecipeListViewHolder(private val view: RecipeListViewBinding) : RecyclerView.ViewHolder(view.root){
+        fun bind(recipe: Recipe) {
+            view.recipe = recipe
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeListViewHolder {
-        TODO("Not yet implemented")
+        return RecipeListViewHolder(RecipeListViewBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: RecipeListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val recipe = viewModel.recipe.value!![position]
+        holder.bind(recipe)
     }
 
     override fun getItemCount() = viewModel.recipe.value!!.size
