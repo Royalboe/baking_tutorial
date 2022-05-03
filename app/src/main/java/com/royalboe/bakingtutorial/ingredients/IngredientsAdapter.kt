7705e9e72a852276c994029c24abe8c276de1cc5
Royalboe <auomidejohnson@gmail.com>
2022-05-03 20:52:35 +0100

    New commits

---
+++ app/src/main/java/com/royalboe/bakingtutorial/ingredients/IngredientsAdapter.kt
@@ -0,0 +1,40 @@
+package com.royalboe.bakingtutorial.ingredients
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.royalboe.bakingtutorial.databinding.IngredientsViewBinding
+import com.royalboe.bakingtutorial.network.Ingredient
+
+class IngredientsAdapter: ListAdapter<Ingredient, IngredientsAdapter.IngredientViewHolder>(
+    DiffUtilCallback
+) {
+    class IngredientViewHolder(val binding: IngredientsViewBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(ingredient: Ingredient) {
+            binding.ingredient = ingredient
+        }
+    }
+
+    companion object DiffUtilCallback: DiffUtil.ItemCallback<Ingredient>() {
+        override fun areItemsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
+            return oldItem.ingredientName == oldItem.ingredientName
+        }
+
+        override fun areContentsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
+           return oldItem.quantity == oldItem.quantity
+        }
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): IngredientsAdapter.IngredientViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: IngredientsAdapter.IngredientViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
