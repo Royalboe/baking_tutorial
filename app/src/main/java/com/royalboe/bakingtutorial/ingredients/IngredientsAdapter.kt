ae72053044dd0030abb5acd6d9c5b1a8dfbc03ff
Royalboe <auomidejohnson@gmail.com>
2022-05-09 23:05:19 +0100

    New commits

--- app/src/main/java/com/royalboe/bakingtutorial/ingredients/IngredientsAdapter.kt
+++ app/src/main/java/com/royalboe/bakingtutorial/ingredients/IngredientsAdapter.kt
@@ -2,33 +2,17 @@ package com.royalboe.bakingtutorial.ingredients
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.royalboe.bakingtutorial.databinding.IngredientsViewBinding
 import com.royalboe.bakingtutorial.network.Ingredient
 import com.royalboe.bakingtutorial.network.Recipe
 
-class IngredientsAdapter(val recipe: Recipe): ListAdapter<Ingredient, IngredientsAdapter.IngredientViewHolder>(
-    DiffUtilCallback
-) {
-    val dataset = recipe.ingredients
+class IngredientsAdapter(val recipe: Recipe): RecyclerView.Adapter<IngredientsAdapter.IngredientViewHolder>() {
+    private val dataset = recipe.ingredients
     class IngredientViewHolder(private val binding: IngredientsViewBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(ingredient: Ingredient) {
             binding.ingredient = ingredient
-            binding.executePendingBindings()
         }
-    }
-
-    companion object DiffUtilCallback: DiffUtil.ItemCallback<Ingredient>() {
-        override fun areItemsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
-            return oldItem.ingredientName == oldItem.ingredientName
-        }
-
-        override fun areContentsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
-           return oldItem.quantity == oldItem.quantity
-        }
-
     }
 
     override fun onCreateViewHolder(
@@ -42,4 +26,6 @@ class IngredientsAdapter(val recipe: Recipe): ListAdapter<Ingredient, Ingredient
         val item = dataset[position]
         holder.bind(item)
     }
+
+    override fun getItemCount() = dataset.size
 }
